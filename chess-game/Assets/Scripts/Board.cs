2f014ea835d5a0a6a51611c498e867940e10245f
jagtym <jagtym@outlook.com>
2021-05-24 18:03:06 +0200

    Add board piece array

--- chess-game/Assets/Scripts/Board.cs
+++ chess-game/Assets/Scripts/Board.cs
@@ -6,9 +6,22 @@ public class Board : MonoBehaviour
 {
     [SerializeField] private Transform bottomLeft;
     [SerializeField] private int squareSize;
+    private Piece[,] boardActiveState;
+    private const int BOARD_SIZE = 8;
+
+    public void Start()
+    {
+        boardActiveState = new Piece[BOARD_SIZE, BOARD_SIZE];
+    }
 
     public Vector3 GetPositionXandY(Vector2Int coordinates)
     {
         return new Vector3(bottomLeft.position.x + coordinates.x * squareSize, bottomLeft.position.y, bottomLeft.position.z + coordinates.y * squareSize);
     }
+
+    public void SetPieceOnBoard(Piece piece)
+    {
+        boardActiveState[piece.currentPosition.x, piece.currentPosition.y] = piece;
+    }
+
 }
