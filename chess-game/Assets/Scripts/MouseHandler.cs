9016f3cae61ff00a5ec7e527b8d7fd926314d692
Buber17 <84589120+Buber17@users.noreply.github.com>
2021-05-24 20:31:42 +0200

    MouseHandling
    fix the variables x and y

---
+++ chess-game/Assets/Scripts/MouseHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+
+public class MouseHandler : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+    private void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            transform.position = raycastHit.point;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            {
+                Debug.Log(mousePos.x);
+                Debug.Log(mousePos.y);
+            }
+        }
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
